4cd1af836059910770d0ad58522f32eef857abad
Rillory Teodora <rilloryteodora@gmail.com>
2025-05-20 22:53:13 -0300

    Js 1 Indo... And maybe voltando

--- templates/JS/home.js
+++ templates/JS/home.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const familia = document.getElementById("linkFamilia");
-
-  if (familia) {
-    familia.style.cursor = "pointer"; // cursor de link
-    familia.addEventListener("click", () => {
-      window.location.href = "categoriaFamilia.html";
-    });
-  }
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaFamilia.html";
+  });
 });
