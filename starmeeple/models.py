2fbc7d6458ede6287e97fd5e7d393cb6c2e04c36
victormurta <68490368+victormurta@users.noreply.github.com>
2025-05-20 21:56:51 -0300

    ajeitando coisas

--- starmeeple/models.py
+++ starmeeple/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class Carrinho(models.Model):
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
-    produto = models.ForeignKey('Produto', on_delete=models.CASCADE)
+    usuario = models.ForeignKey('etabuleiros.Usuario', on_delete=models.CASCADE)
+    produto = models.ForeignKey('etabuleiros.Produto', on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
     data = models.DateTimeField(default=timezone.now) 
+    
