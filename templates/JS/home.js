401b926b14d43bc72db8cb89dc5650cc8caebeb9
Rillory Teodora <rilloryteodora@gmail.com>
2025-05-21 10:11:58 -0300

    Caterias Indo e Voltando

--- templates/JS/home.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const familia = document.getElementById("linkFamilia");
-  familia.addEventListener("click", () => {
-    window.location.href = "categoriaFamilia.html";
-  });
-});
---
+++ templates/JS/padrao.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("linkFamilia");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaFamilia.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("linkJogosCartas");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaJogosCartas.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("linkJogosTabuleiro");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaJogosTabuleiros.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("linkQuebraCabecas");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaQuebraCabeca.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("linkRPG");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaRPG.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const familia = document.getElementById("link18");
+  familia.addEventListener("click", () => {
+    window.location.href = "categoriaProibidao.html";
+  });
+});
