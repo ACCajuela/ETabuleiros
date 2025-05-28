15e261724df71648329c7390c84fd55d790e3450
ACCajuela <aninhacajuela@gmail.com>
2025-05-28 10:56:05 -0300

    teste

--- etabuleiros/tests.py/model_test.py
+++ etabuleiros/tests.py/model_test.py
@@ -1,4 +1,17 @@
+
 import pytest
-from etabuleiros.models import Produto
+from etabuleiros.models import Usuario, Produto
 
 @pytest.mark.django_db
+def test_criar_usuario():
+    usuario = Usuario.objects.create(nome="Pantaleão", email="pantaleao@gmail.com", cpf="333.333.333-3", dataNasc=180219999, telefone = "(19) 99871-4356", endereco="rua galvao")
+    assert usuario.nome == "Pantaleão"
+    assert usuario.email == "pantaleao@gmail.com"
+    assert usuario.cpf == "333.333.333-3"
+    assert usuario.dataNasc == 180219999
+    assert usuario.telefone == "(19) 99871-4356"
+    assert usuario.endereco == "rua galvao"
+
+def test_criar_produto():
+    produto = Produto.objetcts.crete(nome="", qtd = , preco = , cat = , editora = , tempo_de_jogo = , numero_jogadores = , itens_inclusos = " ", descri = " ", recomendado = , clas_ind = , autor = " ", categoria = " ", data_criacao = )
+    assert produto.nome == 
